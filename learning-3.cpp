fe7e6f6b0e57c32aba6a092b8feedeb3b694447d
WLY0807 <1837177383@qq.com>
2026-05-26 13:33:18 +0800

    加注释版

--- learning-3.cpp
+++ learning-3.cpp
@@ -9,4 +9,4 @@ int main(){
     }
     std::cout << "Sum of numbers from 1 to " << b << " is: " << sum << std::endl;   
     return 0;
-}
+}//这个程序主要是算任意你给出的正整数的1到这个数的和，比如你输入5，那么程序就会计算1+2+3+4+5的和，并输出结果15。
