5af8aa6970dc300c3e44de2079e7c8b86a3cd11c
WLY0807 <1837177383@qq.com>
2026-06-08 20:07:13 +0800

    作业9-作用域

--- learning-9.cpp
+++ learning-9.cpp
@@ -2,8 +2,8 @@
 
 int reused = 42; // reuesd拥有全局作用域
 int main() {
-    int unique = 0; // unique拥有main函数的局部作用域；输出42 0
-    std::cout << reused << " " << unique << std::endl;
+    int unique = 0; 
+    std::cout << reused << " " << unique << std::endl;// unique拥有main函数的局部作用域；输出42 0
     
     int reused = 0; // 这里重新定义了一个局部变量reused，覆盖了全局变量reused；输出0 0
     std::cout << reused << " " << unique << std::endl;
